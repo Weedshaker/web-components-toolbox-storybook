36c7af202ec50e8fc827eeaf309788a49854ab92
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-10-12 10:51:05 +0200

    chore: add .babelrc.json configuration file chore: update .storybook/main.js configuration file chore: update devDependencies in package.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,14 +4,20 @@ module.exports = {
     to: '/src'
   }],
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@etchteam/storybook-addon-css-variables-theme', '@storybook/addon-links', {
-    name: '@storybook/addon-essentials',
-    options: {
-      backgrounds: false,
-      measure: false,
-      outline: false
-    }
-  }, 'storybook-addon-themes'],
+  addons: [
+    '@etchteam/storybook-addon-css-variables-theme',
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+        measure: false,
+        outline: false
+      }
+    },
+    'storybook-addon-themes',
+    '@storybook/addon-mdx-gfm'
+  ],
   framework: {
     name: '@storybook/web-components-webpack5',
     options: {}
